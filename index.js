64a6e72ee94168aa61f237cdd073674ab85ee747
octaviohmb <octaviohmb@gmail.com>
2021-10-13 19:20:18 -0300

    added express server hosting index.html

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world!')
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
