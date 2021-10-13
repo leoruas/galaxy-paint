918159e4d4f8a4d12be4d8e8c9a42ae312b6fa98
octaviohmb <octaviohmb@gmail.com>
2021-10-13 19:40:30 -0300

    new route for controller

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.sendFile('index.html')
 })
 
+app.get('/controller', (req, res) => {
+  res.sendFile(__dirname + '/public/controller/controller.html')
+})
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
