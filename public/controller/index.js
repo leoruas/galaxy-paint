a90e5c0043302250165beba3990ab1df3b990654
leoruas <leo.coelho.ruas@gmail.com>
2021-10-20 20:26:11 -0300

    fixed canvas drawing bug

--- public/controller/index.js
+++ public/controller/index.js
@@ -1,7 +1,7 @@
 var socket = io();
 var nScreens; //number of screens
-const canvas = document.getElementById('canvas')
-const body = document.getElementById('body')
+const canvas = document.getElementById('canvas') //canvas reference
+const ctx = canvas.getContext('2d')
 
 /**
  * On new screen method -> responsible for setting variables for screen on connection and calling init function
@@ -15,45 +15,46 @@ function onNewScreen(payload) {
 socket.on('newScreen', onNewScreen)
 
 function init() {
+    //canvas setup
     canvas.style = 'border: 1px solid black'
     const canvasH = window.innerHeight * 0.6
     const canvasW = canvasH * 9 / 16
     canvas.width = canvasW * nScreens
     canvas.height = canvasH
 
-    const ctx = canvas.getContext('2d');
-
+    //brush variables
     var radius = 5;
-        var start = 0;
-        var end = Math.PI * 2;
-        var dragging = false;
+    var start = 0;
+    var end = Math.PI * 2;
+    var dragging = false;
+    ctx.lineWidth = radius * 2;
 
-        ctx.lineWidth = radius * 2;
-
-        function putPoint(e) {
-            if (dragging) {
-                ctx.fillStyle = 'black';
-                ctx.lineTo(e.offsetX, e.offsetY);
-                ctx.stroke();
-                ctx.beginPath();
-                ctx.arc(e.offsetX, e.offsetY, radius, start, end);
-                ctx.fill();
-                ctx.beginPath();
-                ctx.moveTo(e.offsetX, e.offsetY);
-            }
-        }
-
-        function engage(e) {
-            dragging = true;
-            putPoint(e);
-        }
-
-        function disengage() {
-            dragging = false;
+    function putPoint(e) {
+        console.log(e.offsetX)
+        if (dragging) {
+            ctx.fillStyle = 'black';
+            ctx.lineTo(e.offsetX, e.offsetY);
+            ctx.stroke();
             ctx.beginPath();
+            ctx.arc(e.offsetX, e.offsetY, radius, start, end);
+            ctx.fill();
+            ctx.beginPath();
+            ctx.moveTo(e.offsetX, e.offsetY);
         }
+    }
 
-        body.addEventListener('mousedown', engage);
-        body.addEventListener('mousemove', putPoint);
-        body.addEventListener('mouseup', disengage);
+    function engage(e) {
+        dragging = true;
+        putPoint(e);
+    }
+
+    function disengage() {
+        dragging = false;
+        ctx.beginPath();
+    }
+
+    canvas.addEventListener('mousedown', engage);
+    canvas.addEventListener('mousemove', putPoint);
+    canvas.addEventListener('mouseup', disengage);
+    canvas.addEventListener('mouseleave', disengage);
 }
